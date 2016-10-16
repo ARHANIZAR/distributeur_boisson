15d1c65b7d376128c276db31f06458089ea11472
Aymane <ayman100ster@gmail.com>
2016-10-16 08:22:20 +0200

    adding index.php

--- non_object_version/index.php
+++ non_object_version/index.php
@@ -1,5 +1,9 @@
 <?php
-
+session_start();
+if(!empty($myListsOfBoissons)){
+    $_SESSION['list'] = $myListsOfBoissons;
+     redirect_to("./distribution.php");
+}
 
 
 $myListsOfBoissons['fanta'] = array(
@@ -67,16 +71,45 @@ $myListsOfBoissons['redBull'] = array(
             <select name="boisson">
             	<?php
             		
-
+            		foreach ( $myListsOfBoissons as $boisson){
+            			
+            			echo "<option value='".$boisson["name"]."'>".$boisson["name"]."</option>";
+            		}
             	?>
                              
             </select>
             <select name="taille">
-            <option value='petite'>petite</option>
-            <option value='moyenne'>moyenne</option>
-            <option value='grande'>grande</option>
+            <option value='petite'>Petite</option>
+            <option value='moyenne'>Moyenne</option>
+            <option value='grande'>Grande</option>
             </select>
-        	<input type="submit" value="Passez ma commande" />
+
+
+            </select>
+            <table>
+                <tr>
+                    <td><img src="images/2euro.png" alt="piece 2 euro"></td>
+                    <td><img src="images/1euro.png" alt="piece 1 euro"></td>
+                    <td><img src="images/50cents.png" alt="piece 50 cents"></td>
+                    <td><img src="images/20cents.png" alt="piece 20 cents"></td>
+                    <td><img src="images/10cents.png" alt="piece 10 cents"></td>
+                    <td><img src="images/5cents.png" alt="piece 5 cents"></td>
+                    <td><img src="images/2cents.png" alt="piece 2 cents"></td>
+                    <td><img src="images/1cents.png" alt="piece 1 cents"></td>
+                </tr>
+                <tr>
+                    <td><input type="checkbox" name="c2euro" value="1"></td>
+                    <td><input type="checkbox" name="c1euro" value="1"></td>
+                    <td><input type="checkbox" name="c50cents" value="1"></td>
+                    <td><input type="checkbox" name="c20cents" value="1"></td>
+                    <td><input type="checkbox" name="c10cents" value="1"></td>
+                    <td><input type="checkbox" name="c5cents" value="1"></td>
+                    <td><input type="checkbox" name="c2cents" value="1"></td>
+                    <td><input type="checkbox" name="c1cents" value="1"></td>
+                    </tr>
+                    <tr>
+            </table>
+            <input type="submit" value="Passez ma commande" />
         </form>
 
 
