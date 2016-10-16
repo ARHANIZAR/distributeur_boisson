e434908171658f57ba5116d818aea1864d14ea3c
HOUSSAM\houssam <sword4ever@gmail.com>
2016-10-16 10:42:36 +0200

    Add the chnage

--- non_object_version/distribution.php
+++ non_object_version/distribution.php
@@ -68,7 +68,47 @@ if( isset($_POST["boisson"]) && isset($_POST["taille"]) ){
 			echo $boisson["slogan"]."</br>";
 			$sommeRendu = $total - $boisson["prix".$taille];
 			echo "-------------------------------</br>";
-			echo "somme rendu ".$sommeRendu;
+			$nb2Euros = 
+			$nb1Euro = 
+			$nb50cent = 
+			$nb20cent = 
+			$nb10cent = 
+			$nb5cent = 
+			$nb2cent = 
+			$nb1cent = 
+			$piecesDisponible = [2,1,0.5,0.2,0.1,0.05,0.02,0.01];
+			$piecesRendu = [0,0,0,0,0,0,0,0];
+			$imageMap = ["2euro", "1euro", "50cents", "20cents", "10cents", "5cents", "2cents", "1cents"];
+			$aRendre = $sommeRendu;
+			$nbElements = sizeof($piecesDisponible);
+			for ($i = 0; $i<$nbElements; $i++){
+				$piecesRendu[$i] = floor($aRendre / $piecesDisponible[$i]);
+				$aRendre =  fmod($aRendre,$piecesDisponible[$i]);
+			}
+						
+			echo "<b>somme rendu ".$sommeRendu.": </b>";
+			
+			echo '<table>
+					<tr>
+						<td><img src="images/2euro.png" alt="piece 2 euro" /></td>
+						<td><img src="images/1euro.png" alt="piece 1 euro" /></td>
+						<td><img src="images/50cents.png" alt="piece 50 cents" /></td>
+						<td><img src="images/20cents.png" alt="piece 20 cents" /></td>
+						<td><img src="images/10cents.png" alt="piece 10 cents" /></td>
+						<td><img src="images/5cents.png" alt="piece 5 cents" /></td>
+						<td><img src="images/2cents.png" alt="piece 2 cents" /></td>
+						<td><img src="images/1cents.png" alt="piece 1 cents" /></td>
+					</tr>
+					<tr>';
+					
+					echo '<td><input type="text" name="count0" size="3" value="'.$piecesRendu[0].'"/></td>';
+					echo '<td><input type="text" name="count1" size="3" value="'.$piecesRendu[1].'"/></td>';
+					echo '<td><input type="text" name="count2" size="3" value="'.$piecesRendu[2].'"/></td>';
+					echo '<td><input type="text" name="count3" size="3" value="'.$piecesRendu[3].'"/></td>';
+					echo '<td><input type="text" name="count4" size="3" value="'.$piecesRendu[4].'"/></td>';
+					echo '<td><input type="text" name="count5" size="3" value="'.$piecesRendu[5].'"/></td>';
+					echo '<td><input type="text" name="count6" size="3" value="'.$piecesRendu[6].'"/></td>';
+					echo '<td><input type="text" name="count7" size="3" value="'.$piecesRendu[7].'"/></td></tr></table>';
 		} else {
 			echo "vous n'avez pas sufisament d'agrgent pour effectuer cette achat";
 		}
